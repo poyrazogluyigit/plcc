bc155138d144bee1443efab548614738a02a2931
poyrazogluyigit <poyrazogluyigit@gmail.com>
2024-06-02 10:20:23 +0300

    wip

--- src/include/header.hpp
+++ src/include/header.hpp
@@ -1,6 +1,27 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <stack>
+#include <typeinfo>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include "llvm/Pass.h"
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Bitstream/BitstreamReader.h>
+#include <llvm/Bitstream/BitstreamWriter.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/Support/raw_ostream.h>
+using namespace llvm;
 // include order matters, dependencies:
 // stmt -> expr, cond
 // cond -> expr
@@ -8,10 +29,11 @@
 #include "CondAST.hpp"
 #include "StmtAST.hpp"
 #include "DeclAST.hpp"
+
 extern std::unique_ptr<LLVMContext> TheContext;
 extern std::unique_ptr<IRBuilder<>> Builder(TheContext);
 extern std::unique_ptr<Module> TheModule;
-extern std::map<std::string, Value *> NamedValues;
+extern std::map<std::string, Value*> NamedValues;
 
 Value *LogErrorV(const char *Str);
 void yyparse();
